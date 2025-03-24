c9a8e2f7ad69f9fd159ea910820aa6a3aa56959f
tapader13 <minhajtapader0@gmail.com>
2025-03-24 04:32:39 -0400

    ts basic variable

---
+++ simpleTypes.ts
@@ -0,0 +1,21 @@
+let str: string = 'minhaj';
+let num: number = 123;
+let active = true;
+let arr: number[] = [1, 2, 3];
+//tuple
+let tupple: [string, boolean] = ['minu', true];
+
+enum Color {
+  R,
+  G,
+  B,
+}
+const color = Color.B;
+// console.log(color);
+
+const val: any = 'minhaj tapader';
+
+function message(): void {
+  console.log('hi');
+}
+// message();
