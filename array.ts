ada35412234a7bc177a898a41d62528513099f66
tapader13 <minhajtapader0@gmail.com>
2025-03-24 05:09:19 -0400

    ts array

---
+++ array.ts
@@ -0,0 +1,8 @@
+let array: readonly string[] = ['1', '2'];
+// array.pop() here push, pop not work
+
+let array1: number[] = [];
+array1.push(1);
+array1.unshift(2);
+console.log(array1);
+// array1.push("3") not push cause it is string
